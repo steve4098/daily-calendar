ee60c52480bb0af920695984dd1ebee4d1d466f6
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-09-08 19:27:07 +0100

    first part of function for save clicks created and check in console log

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,25 @@
+//displays date in header of page
 let today = dayjs().format("ddd D MMM YYYY");
-
 $("#currentDay").text(today)
 
+//current time called in hour/minute/second
 let currentTime = dayjs().format("HH:mm:ss");
-
 let hourBlock = $("timeblock");
 
+// if (hourBlock.val > currentTime[0]){
+//     $(this).addClass('future');
+// } else if (hourBlock.val<currentTime[0]){
+//     $(this).addClass('past');  
+// }else {
+//     $(this).addClass('present')
+// }
 
-if (hourBlock.val > currentTime[0]){
-    $(this).addClass('future');
-} else if (hourBlock.val<currentTime[0]){
-    $(this).addClass('past');  
-}else {
-    $(this).addClass('present')
-}
+//function for save clicks
+$(document).ready(function() {
+
+
+$(".saveBtn").on("click",function(){
+    
+});
+
+});
